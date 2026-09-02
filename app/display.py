d03fef83be538f226c327b947471878004cc0e0e
Moritz Joekel <moritz@joekel.dev>
2026-09-02 21:38:13 +0200

    Bright midline fix

--- app/display.py
+++ app/display.py
@@ -1,4 +1,7 @@
+import array
 import time
+
+import machine
 from machine import Pin
 
 from app import settings
@@ -9,6 +12,10 @@ except ImportError:
     class _MicroPythonCompat:
         @staticmethod
         def native(func):
+            return func
+
+        @staticmethod
+        def viper(func):
             return func
 
     micropython = _MicroPythonCompat()
@@ -64,6 +71,34 @@ class Hub75Display:
         self.front_frame = bytearray(self.width * self.height)
         self.back_frame = bytearray(self.width * self.height)
 
+        self._scan_words = array.array("I", [0] * (self.width * self.scan_rows))
+
+        self._top_rgb_mask = (1 << settings.R1_PIN) | (1 << settings.G1_PIN) | (1 << settings.B1_PIN)
+        self._bot_rgb_mask = (1 << settings.R2_PIN) | (1 << settings.G2_PIN) | (1 << settings.B2_PIN)
+        self._rgb_mask = self._top_rgb_mask | self._bot_rgb_mask
+        self._clk_mask = 1 << settings.CLK_PIN
+        self._lat_mask = 1 << settings.LAT_PIN
+        self._oe_mask = 1 << settings.OE_PIN
+
+        self._row_mask_all = 0
+        for idx in range(settings.ROWSEL_N_PINS):
+            self._row_mask_all |= 1 << (settings.ROWSEL_BASE_PIN + idx)
+
+        self._row_masks = array.array("I", [0] * self.scan_rows)
+        for row in range(self.scan_rows):
+            row_mask = 0
+            for bit_idx in range(settings.ROWSEL_N_PINS):
+                if row & (1 << bit_idx):
+                    row_mask |= 1 << (settings.ROWSEL_BASE_PIN + bit_idx)
+            self._row_masks[row] = row_mask
+
+        self._gpio_set_addr = settings.SIO_GPIO_OUT_SET
+        self._gpio_clr_addr = settings.SIO_GPIO_OUT_CLR
+        self._mem32 = getattr(machine, "mem32", None)
+        self._use_fast_gpio = settings.USE_FAST_GPIO_SCAN and self._mem32 is not None
+
+        self._rebuild_scan_words()
+
     def clear_frame(self, buf):
         for i in range(len(buf)):
             buf[i] = 0
@@ -100,15 +135,80 @@ class Hub75Display:
     def show_text(self, text):
         self.draw_text_center(self.back_frame, text, self.text_scale)
         self.front_frame, self.back_frame = self.back_frame, self.front_frame
+        self._rebuild_scan_words()
 
     @micropython.native
-    def scan_frame_once(self):
-        r1_value = self.r1.value
-        g1_value = self.g1.value
-        b1_value = self.b1.value
-        r2_value = self.r2.value
-        g2_value = self.g2.value
-        b2_value = self.b2.value
+    def _rebuild_scan_words(self):
+        frame_local = self.front_frame
+        scan_words = self._scan_words
+        width = self.width
+        scan_rows = self.scan_rows
+        top_rgb_mask = self._top_rgb_mask
+        bot_rgb_mask = self._bot_rgb_mask
+
+        for row in range(scan_rows):
+            top_index = row * width
+            bot_index = (row + scan_rows) * width
+            row_index = row * width
+
+            for x in range(width):
+                top_on = frame_local[top_index + x]
+                bot_on = frame_local[bot_index + x]
+                scan_words[row_index + x] = (top_rgb_mask if top_on else 0) | (bot_rgb_mask if bot_on else 0)
+
+    @micropython.native
+    def _scan_frame_once_fast(self):
+        mem32 = self._mem32
+        gpio_set = self._gpio_set_addr
+        gpio_clr = self._gpio_clr_addr
+        rgb_mask = self._rgb_mask
+        clk_mask = self._clk_mask
+        lat_mask = self._lat_mask
+        oe_mask = self._oe_mask
+        row_mask_all = self._row_mask_all
+        row_masks = self._row_masks
+        scan_words = self._scan_words
+        width = self.width
+        scan_rows = self.scan_rows
+        sleep_us = time.sleep_us
+        on_time_us = self.on_time_us
+
+        for row in range(scan_rows):
+            mem32[gpio_set] = oe_mask
+
+            mem32[gpio_clr] = row_mask_all
+            mem32[gpio_set] = row_masks[row]
+
+            row_index = row * width
+            for x in range(width):
+                mem32[gpio_clr] = rgb_mask
+                mem32[gpio_set] = scan_words[row_index + x]
+                mem32[gpio_set] = clk_mask
+                mem32[gpio_clr] = clk_mask
+
+            mem32[gpio_set] = lat_mask
+            mem32[gpio_clr] = lat_mask
+
+            mem32[gpio_clr] = oe_mask
+            sleep_us(on_time_us)
+
+        # Keep output blanked between frame scans to avoid row hold artifacts.
+        mem32[gpio_set] = oe_mask
+
+    @micropython.native
+    def _scan_frame_once_compat(self):
+        r1_on = self.r1.on
+        r1_off = self.r1.off
+        g1_on = self.g1.on
+        g1_off = self.g1.off
+        b1_on = self.b1.on
+        b1_off = self.b1.off
+        r2_on = self.r2.on
+        r2_off = self.r2.off
+        g2_on = self.g2.on
+        g2_off = self.g2.off
+        b2_on = self.b2.on
+        b2_off = self.b2.off
         row0_value = self.row_pins[0].value
         row1_value = self.row_pins[1].value
         row2_value = self.row_pins[2].value
@@ -123,7 +223,9 @@ class Hub75Display:
         width = self.width
         scan_rows = self.scan_rows
         on_time_us = self.on_time_us
-        frame_local = self.front_frame
+        scan_words = self._scan_words
+        top_mask = self._top_rgb_mask
+        bot_mask = self._bot_rgb_mask
 
         for row in range(scan_rows):
             oe_on()
@@ -133,19 +235,28 @@ class Hub75Display:
             row2_value((row >> 2) & 0x01)
             row3_value((row >> 3) & 0x01)
 
-            top_index = row * width
-            bot_index = (row + scan_rows) * width
-
+            row_index = row * width
             for x in range(width):
-                top_on = frame_local[top_index + x]
-                bot_on = frame_local[bot_index + x]
+                rgb_bits = scan_words[row_index + x]
 
-                r1_value(top_on)
-                g1_value(top_on)
-                b1_value(top_on)
-                r2_value(bot_on)
-                g2_value(bot_on)
-                b2_value(bot_on)
+                if rgb_bits & top_mask:
+                    r1_on()
+                    g1_on()
+                    b1_on()
+                else:
+                    r1_off()
+                    g1_off()
+                    b1_off()
+
+                if rgb_bits & bot_mask:
+                    r2_on()
+                    g2_on()
+                    b2_on()
+                else:
+                    r2_off()
+                    g2_off()
+                    b2_off()
+
                 clk_on()
                 clk_off()
 
@@ -154,6 +265,17 @@ class Hub75Display:
 
             oe_off()
             sleep_us(on_time_us)
+
+        # Keep output blanked between frame scans to avoid row hold artifacts.
+        oe_on()
+
+    @micropython.native
+    def scan_frame_once(self):
+        if self._use_fast_gpio:
+            self._scan_frame_once_fast()
+            return
+
+        self._scan_frame_once_compat()
 
     def scan_batch(self, count):
         for _ in range(count):
